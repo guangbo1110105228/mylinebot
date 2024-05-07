a24ae0b4b0953a9c6f2637b3b536ac41c0bae7ef
Jan <jan0984327881@gmail.com>
2024-05-07 15:07:56 +0800

    2024/5/7

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-import traceback
-#======python的函數庫==========
-
-
 import json
+import openai
+import traceback
+import os
+
+# LINE BOT info
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
-# LINE BOT info
-line_bot_api = LineBotApi('Channel Access Token')
-handler = WebhookHandler('Channel Secret')
-openai.api_key = ('YOUR_OPENAI_API_KEY')
-
 
 def GPT_response(text):
-    # 接收回應
+    # Generate response using OpenAI
     response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
     return answer
 
@@ -54,8 +42,6 @@ def handle_follow(event):
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    print(body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -65,8 +51,6 @@ def callback():
 # Message event
 @handler.add(MessageEvent)
 def handle_message(event):
-    message_type = event.message.type
-    user_id = event.source.user_id
     reply_token = event.reply_token
     message = event.message.text
 
@@ -82,9 +66,6 @@ def handle_message(event):
         print(traceback.format_exc())
         line_bot_api.reply_message(reply_token, TextSendMessage('An error occurred, please check the logs for more details.'))
 
-
-
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
